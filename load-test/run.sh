a19f6d8888d30f160fb1c936dcfc072af750b626
leejinwoo <leejinwoo1126@gmail.com>
2024-09-12 14:19:26 +0900

    locust script 추가, 수정

--- load-test/run.sh
+++ load-test/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 HOST_IP=$(hostname -I | awk '{print $1}')
 
+TARGET_HOST="http://$HOST_IP:8080" docker-compose down
+
 TARGET_HOST="http://$HOST_IP:8080" docker-compose up -d --scale worker=3

eaaaf515c3e7bcaf2d4e9c07867c0ddff532157c
leejinwoo <leejinwoo1126@gmail.com>
2024-10-14 21:43:39 +0900

    refactor: Kafka publisher Headers 문자열 수정

--- coupon-core/src/main/java/com/example/couponcore/service/event/CouponIssuePublisher.java
+++ coupon-core/src/main/java/com/example/couponcore/service/event/CouponIssuePublisher.java
@@ -1,7 +1,9 @@
 package com.example.couponcore.service.event;
 
+import org.springframework.kafka.support.KafkaHeaders;
+
 public interface CouponIssuePublisher {
-    String MESSAGE_KEY = "custom.messageKey";
+    String MESSAGE_KEY = KafkaHeaders.KEY;
 
     void publish(CouponIssueEvent event);
 }
